3e7cb53e9292546d9d478e4662a1399f12787d07
Renzo Crisóstomo <renzo.crisostomomedina@here.com>
2018-04-03 14:08:41 +0200

    Initial commit

---
+++ ROCM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ROCM
+//
+//  Created by Crisostomo Medina, Renzo on 03.04.18.
+//  Copyright © 2018 Crisostomo Medina, Renzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
